f3d2822062192a440c9aec9a853ddefa2b9c6680
fiqriAidin <fikriaidin98@gmail.com>
2022-10-18 19:58:18 +0700

    memperbaiki bug halaman ruang

--- lib/pages/list_ruang.dart
+++ lib/pages/list_ruang.dart
@@ -11,112 +11,32 @@ class ListRuang extends StatefulWidget {
 }
 
 class _ListRuangState extends State<ListRuang> {
+  List<String> field = ["Semua Ruang", "Ruang Open", "Ruang Close"];
+  String _field = "Semua Ruang";
+  var newData = ([]);
+  var oldData = ([]);
   var itemCount = 0;
 
   @override
   void initState() {
-    // print("Regres hal ruang");
+    Future.delayed(const Duration(milliseconds: 1300), () {
+      setState(() {
+        newData = oldData;
+      });
+      // print(newData);
+    });
+
+    // TODO: implement initState
     super.initState();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("List Ruang"),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(25),
-              bottomRight: Radius.circular(25)),
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                  return const Login();
-                }));
-              },
-              icon: const Icon(
-                Icons.logout,
-                color: Colors.white,
-              ))
-        ],
-      ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          await Future.delayed(Duration(milliseconds: 1500));
-          setState(() {
-            itemCount = itemCount + 1;
-          });
-        },
-        child: FutureBuilder(
-          future: getDataRuang(),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              print(snapshot.error);
-            }
+  String convertDate(time) {
+    var date = DateTime.fromMillisecondsSinceEpoch(int.parse(time));
+    var result = "${date.day}-${date.month}-${date.year}";
 
-            return snapshot.hasData
-                ? ListValue(
-                    list: snapshot.data,
-                  )
-                : const Center(
-                    child: CircularProgressIndicator(),
-                  );
-          },
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: const Icon(Icons.add),
-        label: const Text("Tambah Ruang"),
-        backgroundColor: Colors.green,
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return CreateRuang();
-          }));
-        },
-      ),
-    );
-  }
-}
-
-class ListValue extends StatefulWidget {
-  ListValue({Key? key, this.list}) : super(key: key);
-  var list;
-
-  @override
-  _ListValueState createState() => _ListValueState();
-}
-
-class _ListValueState extends State<ListValue> {
-  List<String> field = ["Semua Ruang", "Ruang Open", "Ruang Close"];
-  String _field = "Semua Ruang";
-  var newData = ([]);
-
-  @override
-  void initState() {
-    newData = widget.list;
-    // print(newData);
-    super.initState();
+    return result;
   }
 
-  void filterData(String value) {
-    var result = [];
-    if (value == "all") {
-      result = widget.list;
-    } else {
-      result = widget.list
-          .where((element) => element["status"]
-              .toString()
-              .toLowerCase()
-              .contains(value.toLowerCase()))
-          .toList();
-    }
-    newData = result;
-  }
-
-  @override
   void confirm(id, name) {
     AlertDialog alertDialog = AlertDialog(
       content: Text("Apakah kamu yakin ingin menghapus ${name}"),
@@ -124,6 +44,7 @@ class _ListValueState extends State<ListValue> {
         ElevatedButton(
           onPressed: () {
             Navigator.of(context).pop();
+            setState(() {});
           },
           child: const Text("Batal"),
           style: ElevatedButton.styleFrom(
@@ -134,6 +55,7 @@ class _ListValueState extends State<ListValue> {
           onPressed: () {
             deleteDataRuang(id);
             Navigator.pop(context);
+            setState(() {});
           },
           child: const Text("Delete"),
           style: ElevatedButton.styleFrom(
@@ -151,11 +73,19 @@ class _ListValueState extends State<ListValue> {
     );
   }
 
-  String convertDate(time) {
-    var date = DateTime.fromMillisecondsSinceEpoch(int.parse(time));
-    var result = "${date.day}-${date.month}-${date.year}";
-
-    return result;
+  void filterData(String value) {
+    var result = [];
+    if (value == "all") {
+      result = oldData;
+    } else {
+      result = oldData
+          .where((element) => element["status"]
+              .toString()
+              .toLowerCase()
+              .contains(value.toLowerCase()))
+          .toList();
+    }
+    newData = result;
   }
 
   void viewDetail(id, name, code, descrition, firsTime, lastTime, status) {
@@ -289,79 +219,146 @@ class _ListValueState extends State<ListValue> {
     );
   }
 
+  Widget listView(BuildContext context) {
+    return Expanded(
+      child: ListView.builder(
+        itemCount: newData == null ? 0 : newData.length,
+        itemBuilder: (context, index) {
+          return Container(
+            padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+            child: Card(
+              color: newData[index]["status"] == "open"
+                  ? Colors.green
+                  : Colors.red,
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20))),
+              child: ListTile(
+                title: Text(newData[index]["name"]),
+                subtitle: Text('Status : ${newData[index]["status"]}'),
+                leading: const Icon(
+                  Icons.meeting_room,
+                  size: 30,
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  viewDetail(
+                    newData[index]["id"],
+                    newData[index]["name"],
+                    newData[index]["code"],
+                    newData[index]["description"],
+                    newData[index]["firstTimeOff"],
+                    newData[index]["lastTimeOff"],
+                    newData[index]["status"],
+                  );
+                },
+                iconColor: Colors.white,
+                textColor: Colors.white,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void temp(var value) {
+    oldData = value;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-          child: DropdownButtonFormField(
-            decoration: InputDecoration(
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0))),
-            value: _field,
-            items: field.map((value) {
-              return DropdownMenuItem(
-                child: Text(value),
-                value: value,
-              );
-            }).toList(),
-            onChanged: (value) {
-              setState(() {
-                _field = value.toString();
-              });
-              if (value.toString() == "Ruang Open") {
-                filterData("open");
-              } else if (value.toString() == "Ruang Close") {
-                filterData("close");
-              } else {
-                filterData("all");
-              }
-            },
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("List Ruang"),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25),
+              bottomRight: Radius.circular(25)),
         ),
-        Expanded(
-          child: ListView.builder(
-            itemCount: newData == null ? 0 : newData.length,
-            itemBuilder: (context, index) {
-              return Container(
-                padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
-                child: Card(
-                  color: newData[index]["status"] == "open"
-                      ? Colors.green
-                      : Colors.red,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20))),
-                  child: ListTile(
-                    title: Text(newData[index]["name"]),
-                    subtitle: Text('Status : ${newData[index]["status"]}'),
-                    leading: Icon(
-                      Icons.meeting_room,
-                      size: 30,
-                    ),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                    onTap: () {
-                      viewDetail(
-                        widget.list[index]["id"],
-                        widget.list[index]["name"],
-                        widget.list[index]["code"],
-                        widget.list[index]["description"],
-                        widget.list[index]["firstTimeOff"],
-                        widget.list[index]["lastTimeOff"],
-                        widget.list[index]["status"],
-                      );
-                    },
-                    iconColor: Colors.white,
-                    textColor: Colors.white,
-                  ),
-                ),
-              );
-            },
-          ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.pushReplacement(context,
+                    MaterialPageRoute(builder: (context) {
+                  return const Login();
+                }));
+              },
+              icon: const Icon(
+                Icons.logout,
+                color: Colors.white,
+              ))
+        ],
+      ),
+      body: RefreshIndicator(
+        onRefresh: () async {
+          await Future.delayed(Duration(milliseconds: 1500));
+
+          setState(() {
+            itemCount = itemCount + 1;
+            newData = oldData;
+          });
+        },
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0))),
+                value: _field,
+                items: field.map((value) {
+                  return DropdownMenuItem(
+                    child: Text(value),
+                    value: value,
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    _field = value.toString();
+                  });
+                  if (value.toString() == "Ruang Open") {
+                    filterData("open");
+                  } else if (value.toString() == "Ruang Close") {
+                    filterData("close");
+                  } else {
+                    filterData("all");
+                  }
+                },
+              ),
+            ),
+            FutureBuilder(
+              future: getDataRuang(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  print(snapshot.error);
+                }
+
+                if (snapshot.hasData) {
+                  temp(snapshot.data);
+                  return listView(context);
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
+            ),
+          ],
         ),
-      ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.add),
+        label: const Text("Tambah Ruang"),
+        backgroundColor: Colors.green,
+        onPressed: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return CreateRuang();
+          }));
+        },
+      ),
     );
   }
 }
